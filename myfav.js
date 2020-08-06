513914860215976c0aa4f4c7493a71cb3521e5da
dejan-77 <dejankutic@me.com>
2020-08-07 00:52:08 +0200

    the real code

--- myfav.js
+++ myfav.js
@@ -1,14 +1,3 @@
-/* SCROLLTRIGGER REVEAL ALL IMAGE IN VIEW WITH CLIPPATH */
 
-document.querySelectorAll("img").forEach(img => {
-    gsap.fromTo(img, {
-      clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
-    }, {
-      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-      duration: 0.6,
-      ease: "power1.inOut",
-      scrollTrigger: img
-    });
-  });
+gsap.to("#box", {duration: 1, rotate:360, yoyo:true, repeat:-1})
 
-  // MRŠ U PIČKU
