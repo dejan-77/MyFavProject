6ba4d402652622b9fc6816d691c909b9744eb420
dejan-77 <dejankutic@me.com>
2020-08-07 00:01:00 +0200

    prvi kod

--- myfav.js
+++ myfav.js
@@ -1,4 +1,14 @@
-Learning git in VSC stage 2
+/* SCROLLTRIGGER REVEAL ALL IMAGE IN VIEW WITH CLIPPATH */
 
+document.querySelectorAll("img").forEach(img => {
+    gsap.fromTo(img, {
+      clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
+    }, {
+      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+      duration: 0.6,
+      ease: "power1.inOut",
+      scrollTrigger: img
+    });
+  });
 
-Ovo su teške izmjene
+  
