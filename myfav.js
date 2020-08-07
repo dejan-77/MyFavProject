2fdd03d61d17eef15b3e2ee69880a6e7eed37bad
dejan-77 <dejankutic@me.com>
2020-08-07 21:49:25 +0200

    next

--- myfav.js
+++ myfav.js
@@ -1,2 +1,5 @@
 
-gsap.to("#box", {duration:32, x:320, y:140, backgroundColor:"green", rotate:10, yoyo: true, repeat:2})
+gsap.to("#box", {duration:320, x:320, y:140, backgroundColor:"green", rotate:10, yoyo: true, repeat:2})
+
+
+second ChannelMergerNode
