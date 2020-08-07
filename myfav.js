55bb3c8950d5867ec28b579327a42bf64ab48f24
dejan-77 <dejankutic@me.com>
2020-08-07 21:12:27 +0200

    ff

--- myfav.js
+++ myfav.js
@@ -1,4 +1,4 @@
 
-gsap.to("#box", {duration: 2, x:-320, y:-300, backgroundColor: "#E2FF0F", rotate:200, yoyo: true, repeat:-1})
+gsap.to("#box", {duration: 3, x:-320, y:-300, backgroundColor: "#E2FF0F", rotate:200, yoyo: true, repeat:-1})
 
 
