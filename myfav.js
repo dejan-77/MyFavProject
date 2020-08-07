142762b7a95ed9843925ec6a8982fec131ca48cd
dejan-77 <dejankutic@me.com>
2020-08-07 21:46:33 +0200

    fisrt commit

--- myfav.js
+++ myfav.js
@@ -1,2 +1,2 @@
 
-gsap.to("#box", {duration:30, x:320, y:140, backgroundColor:"#E2FF0F", rotate:200, yoyo: true, repeat:2})
+gsap.to("#box", {duration:32, x:320, y:140, backgroundColor:"#E2FF0F", rotate:200, yoyo: true, repeat:2})
