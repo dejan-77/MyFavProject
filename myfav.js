1b3f78341ead763863f8ebe8c750741fe9b21329
dejan-77 <dejankutic@me.com>
2020-08-08 14:24:59 +0200

    updated color

--- myfav.js
+++ myfav.js
@@ -1,2 +1,9 @@
 
-gsap.to("#box", {duration:3, x:320, y:140, backgroundColor:"green", rotate:10, yoyo: true, repeat:2})
+gsap.to("#box", {
+    duration:3, 
+    x:320, 
+    y:140, 
+    backgroundColor:"#A8C9D4", 
+    rotate:10, 
+    yoyo: true, 
+    repeat:2})
