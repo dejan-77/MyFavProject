482c5f03389d46c0dc6b7246cb8edf69116f3f8f
dejan-77 <dejankutic@me.com>
2020-08-08 17:00:23 +0200

    valjda nesto

--- myfav.js
+++ myfav.js
@@ -3,7 +3,7 @@ gsap.to("#box", {
     duration:3, 
     x:-320, 
     y:-139, 
-    backgroundColor:"#D51847", 
+    backgroundColor:"#ECEED9", 
     rotate:300, 
    // yoyo: true, 
     repeat:-1})
